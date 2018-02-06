1f82f3bcf3ac5be54880bf27bd5052d31099d936
andreasaksono <saksonoandrea102@gmail.com>
2018-02-05 19:52:21 -0800

    Add files via upload

---
+++ week-4/config/index.js
@@ -0,0 +1,9 @@
+module.exports = {  
+    // Database connection information
+    'database_mlab': 'mongodb://asaksono:mongodb@ds123698.mlab.com:23698/countriesdb',
+    'database_local': 'countrydb',
+
+    // Setting port for server
+    'port': process.env.PORT || 3000   
+};
+
